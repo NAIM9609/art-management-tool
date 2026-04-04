f50f87cef0d49d72cb8f321d8320e939b41a62a1
Naimmo <samuel.haddar@baxenergy.com>
2026-04-04 16:32:38 +0200

    (terraform): change cors origins

--- infrastructure/services/common-variables.tf
+++ infrastructure/services/common-variables.tf
@@ -5,5 +5,5 @@
 variable "allowed_origins" {
   description = "List of origins allowed to call service HTTP APIs (CORS). Restrict to trusted domains in production."
   type        = list(string)
-  default     = ["http://localhost:3000"]
+  default     = ["https://test.giorgiopriviteralab.com","https://giorgiopriviteralab.com"]
 }
